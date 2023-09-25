898070ba3fe1b438016b7bd6c92f544e78683699
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-09-25 14:57:41 +0530

    feat - started with snake game in typescript

---
+++ src/components/Game.tsx
@@ -0,0 +1,50 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React, { JSX, useState } from "react";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import { Colors } from "../globals/colors";
+import { Coordinate, Direction, GestureEventType } from "../types/types";
+
+const SNAKE_INITIAL_POSITION = [{ x: 5, y: 5 }];
+const FOOD_INITIAL_POSITION = { x: 5, y: 20 };
+const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax: 63 };
+const MOVE_INTERVAL = 50;
+const SCORE_INCREMENT = 10;
+
+const Game = (): JSX.Element => {
+  const [direction, setDirection] = useState<Direction>(Direction.Right);
+  const [snake, setSnake] = useState<Coordinate[]>(SNAKE_INITIAL_POSITION);
+  const [isGameOver, setIsGameOver] = useState<boolean>(false);
+  const [isGamePaused, setIsGamePaused] = useState<boolean>(false);
+  
+
+  const handleGesture = (event: GestureEventType) => {
+    const { translationX, translationY } = event.nativeEvent;
+    if (Math.abs(translationX) > Math.abs(translationY)) {
+      if (translationX > 0) {
+        // moving right
+      } else {
+        //moving left
+      }
+    } else {
+      if (translationY > 0) {
+        // moving down
+      } else {
+        //moving up
+      }
+    }
+  };
+  return (
+    <PanGestureHandler onGestureEvent={handleGesture}>
+      <SafeAreaView style={styles.container}></SafeAreaView>
+    </PanGestureHandler>
+  );
+};
+
+export default Game;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.primary,
+    flex: 1,
+  },
+});
